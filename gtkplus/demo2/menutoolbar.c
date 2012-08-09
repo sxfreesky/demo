9c246ea5c10c2b0b6acf7584f3100805a2fa2051
sxfreesky <sxfreesky@gmail.com>
2012-08-09 15:50:08 +0800

    学习第二节 快速添加菜单

--- gtkplus/demo2/menutoolbar.c
+++ gtkplus/demo2/menutoolbar.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 
 void menu(int argc, char* argv[]);
+void itemfact(int argc, char* argv[]);
 
 char* txt(char* c)
 {
@@ -9,7 +10,7 @@ char* txt(char* c)
 
 int main(int argc, char* argv[])
 {
-    menu(argc, argv);
+    itemfact(argc, argv);
     return FALSE;
 }
 
@@ -109,5 +110,50 @@ void menu(int argc, char* argv[])
     gtk_menu_shell_append(GTK_MENU_SHELL(menubar), rootmenu);
     gtk_box_pack_start(GTK_BOX(box), menubar, FALSE, FALSE, 0);
     gtk_widget_show_all(window);
+    gtk_main();
+}
+
+static GtkItemFactoryEntry menu_items[]={   //定义菜单集
+    {"/文件(_F)",NULL,NULL,0,"<Branch>"},
+    {"/文件(_F) /新建",NULL,on_menu_activate,"新建","<StockItem>", GTK_STOCK_NEW},
+    {"/文件(_F) /打开",NULL,on_menu_activate,"打开","<StockItem>", GTK_STOCK_OPEN},
+    {"/文件(_F) /保存",NULL,on_menu_activate,"保存","<StockItem>", GTK_STOCK_SAVE},
+    {"/文件(_F) /另存为",NULL,on_menu_activate,"另存为","<StockItem>", GTK_STOCK_SAVE_AS},
+    {"/文件(_F) /-",NULL,on_menu_activate,NULL,"<Seprator>"},
+    {"/文件(_F) /退出",NULL,on_menu_activate,"退出","<StockItem>", GTK_STOCK_QUIT},
+
+    {"/编辑(_E)",NULL,NULL,0,"<Branch>"},
+    {"/编辑(_E) /剪切",NULL,on_menu_activate,"剪切","<StockItem>", GTK_STOCK_CUT},
+    {"/编辑(_E) /复制",NULL,on_menu_activate,"复制","<StockItem>", GTK_STOCK_COPY},
+    {"/编辑(_E) /粘贴",NULL,on_menu_activate,"粘贴","<StockItem>", GTK_STOCK_PASTE},
+    {"/编辑(_E) /查找",NULL,on_menu_activate,"查找","<StockItem>", GTK_STOCK_FIND},
+    {"/帮助(_H)",NULL,NULL,0,"<LastBranch>"},
+    {"/帮助(_H) /帮助",NULL,on_menu_activate,"帮助","<StockItem>", GTK_STOCK_HELP},
+    {"/帮助(_H) /关于...",NULL,on_menu_activate,"关于",NULL}
+};
+void itemfact(int argc, char* argv[])
+{
+    GtkWidget* window;
+    GtkWidget* box;
+    GtkWidget* menubar;
+    GtkAccelGroup* accel_group;
+    GtkItemFactory* item_factory;
+    gint n=15;
+
+    gtk_init(&argc, &argv);
+    window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), txt("添加菜单的快捷方法"));
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    accel_group=gtk_accel_group_new();
+    box=gtk_vbox_new(FALSE,0);
+    gtk_container_add(GTK_CONTAINER(window), box);
+    gtk_widget_show(box);
+    item_factory=gtk_item_factory_new(GTK_TYPE_MENU_BAR, "<main>", accel_group);
+    gtk_item_factory_create_items(item_factory, n, menu_items, NULL);
+    gtk_window_add_accel_group(GTK_WINDOW(window), accel_group);
+    menubar=gtk_item_factory_get_widget(item_factory, "<main>");
+    gtk_box_pack_start(GTK_BOX(box), menubar, FALSE, FALSE, 0);
+    gtk_widget_show(menubar);
+    gtk_widget_show(window);
     gtk_main();
 }
